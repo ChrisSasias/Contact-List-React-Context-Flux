6e4f305cbb5cf6e18936636ca008b0a7c40ed909
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-12-04 19:12:46 +0000

    create contact in the agenda

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import { Link, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+
+	const navigate = useNavigate();
+
+
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
+
+			<button className="btn btn-primary" onClick={() => { navigate("/add") }}>navigate</button>
+
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<Link to="/add" className="btn btn-primary">
+					<span>Add contact</span>
 				</Link>
 			</div>
 		</nav>
---
+++ src/js/views/Add.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import "../../styles/home.css";
+
+export const Add = () => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [address, setAddress] = useState("");
+
+	const URL_API = "https://playground.4geeks.com/apis/fake/contact/";
+
+	const handleChange = (event, set) => {
+		set(event.target.value);
+
+	};
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		try {
+			const response = await fetch(URL_API, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					full_name: name,
+					email: email,
+					agenda_slug: "Agenda_Chris",
+					address: address,
+					phone: phone,
+				}),
+			});
+
+			if (response.ok) {
+				const data = await response.json();
+				console.log("Data sent successfully:", data);
+				setName("");
+				setEmail("");
+				setPhone("");
+				setAddress("");
+			} else {
+				console.error("Failed to send data:", response.statusText, response.status );
+			}
+		} catch (err) {
+			console.error("An error occurred:", err);
+		}
+	};
+
+	return (
+		<div className="text-center mt-5 m-4 m-4 border-5">
+			<div>
+				<h2>Add a new contact</h2>
+				<form onSubmit={handleSubmit}>
+					<div className="form-group">
+						<label htmlFor="fullName" className="d-flex flex-row mt-2 m-1">
+							Full Name
+						</label>
+						<input
+							type="text"
+							className="form-control"
+							id="fullName"
+							placeholder="Full Name"
+							onChange={(e) => handleChange(e, setName)}
+							value={name}
+						/>
+					</div>
+					<div className="form-group">
+						<label
+							htmlFor="EMAIL"
+							className="d-flex flex-row mt-2 m-1"
+						>
+							Email
+						</label>
+						<input
+							type="email"
+							className="form-control"
+							id="EMAIL"
+							placeholder="Enter email"
+							onChange={(e) => handleChange(e, setEmail)}
+							value={email}
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="phone" className="d-flex flex-row mt-2 m-1">
+							Phone
+						</label>
+						<input
+							type="number"
+							className="form-control"
+							id="phone"
+							placeholder="Enter Phone"
+							onChange={(e) => handleChange(e, setPhone)}
+							value={phone}
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="address" className="d-flex flex-row mt-2 m-1">
+							Address
+						</label>
+						<input
+							type="text"
+							className="form-control"
+							id="address"
+							placeholder="Enter address"
+							onChange={(e) => handleChange(e, setAddress)}
+							value={address}
+						/>
+					</div>
+
+					<button type="submit" className="btn btn-primary w-100 mt-4">
+						Save
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+};
